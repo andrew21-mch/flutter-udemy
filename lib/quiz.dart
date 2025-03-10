ed8c46a01c6c5eadfc41287c475ab86a336ff135
Snr Andy <nfonandrew73@gmail.com>
2025-03-10 08:02:33 +0100

    alternative-2

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,36 +12,41 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-
   var activeScreen = "start-screen";
-
 
   // @override
   // void initState() {
   //   activeScreen = StartScreen(switchScreen);
   //   super.initState();
   // }
-  void switchScreen(){
+  void switchScreen() {
     setState(() {
       activeScreen = "question-screen";
     });
   }
+
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'question-screen') {
+      screenWidget = QuestionsScreen();
+    }
     return MaterialApp(
       home: Scaffold(
         body: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 10, 46, 109),
-                  const Color.fromARGB(255, 2, 94, 117)
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 10, 46, 109),
+                const Color.fromARGB(255, 2, 94, 117)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            child: activeScreen == 'start-screen' ? StartScreen(switchScreen): QuestionsScreen()),
+          ),
+          child: screenWidget,
+        ),
       ),
     );
   }
